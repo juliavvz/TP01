65009986c270d46768ea8abd997c065c86cfde63
Júlia <juliavvitoria376@gmail.com>
2026-03-20 15:09:05 -0300

    Ex15: conversão dólar para real

--- TP01Ex15.java
+++ TP01Ex15.java
@@ -2,3 +2,26 @@
  quantidade de dólares. Calcular e exibir o valor correspondente em Reais (R$).
  
  Dupla: Júlia Vitória e Lucas Antônio*/
+
+ import java.util.Scanner;
+
+  public class TP01Ex15 {   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("valor (US$):");
+        double v1 = scanner.nextDouble();
+
+        System.out.println("valor cotação:");
+        double a = scanner.nextDouble();
+
+        double vr = v1 * a;
+
+        System.out.println(vr);
+
+    
+
+        scanner.close();
+    }
+}
