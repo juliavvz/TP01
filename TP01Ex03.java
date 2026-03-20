777f703ce3317ffe46ad602af9ab5b14658fdd67
Júlia <juliavvitoria376@gmail.com>
2026-03-20 12:29:02 -0300

    Ex03: área do quadrado pela diagonal

--- TP01Ex03.java
+++ TP01Ex03.java
@@ -2,3 +2,23 @@
   de sua diagonal que será digitado.
   
   Dupla: Júlia Vitória e Lucas Antônio*/
+
+
+  import java.util.Scanner;
+
+  public class TP01Ex03 {   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("digite diagonal do quadrado:");
+        double d = scanner.nextDouble();
+        
+        double r = Math.pow(d, 2)/2;
+
+        System.out.println(r);
+        
+        
+        scanner.close();
+    }
+}
