fac12f1c1febd0edef0e9469043749910e01aaea
Júlia <juliavvitoria376@gmail.com>
2026-03-20 14:58:50 -0300

    Ex13: velocidade final

--- TP01Ex13.java
+++ TP01Ex13.java
@@ -3,3 +3,30 @@
  e do tempo de percurso (em s) que serão digitados. 
  
  Dupla: Júlia Vitória e Lucas Antônio*/
+
+
+ import java.util.Scanner;
+
+  public class TP01Ex13 {   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Digite velocidade inicial(m/s):");
+        double v0 = scanner.nextDouble();
+
+        System.out.println("Digite valor da aceleração(m/s²):");
+        double a = scanner.nextDouble();
+
+        System.out.println("Digite valor do tempo(s):");
+        double t = scanner.nextDouble();
+
+       double vms = v0 + (a * t);
+       double vf = vms * 3.6;
+
+
+        System.out.println(vf);
+
+        scanner.close();
+    }
+}
