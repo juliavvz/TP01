be28195a26e60a2f9a4d9b344d634fe5d16cfdae
Júlia <juliavvitoria376@gmail.com>
2026-03-20 14:05:03 -0300

    Ex11: área do círculo pelo diâmetro

--- TP01Ex11.java
+++ TP01Ex11.java
@@ -2,3 +2,23 @@
   calcular e exibir sua área. 
   
   Dupla: Júlia Vitória e Lucas Antônio*/
+
+  import java.util.Scanner;
+
+  public class TP01Ex11 {   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Digite valor do diâmetro:");
+        double d = scanner.nextDouble();
+
+        double r = d / 2;
+
+        double a = 3.14 * Math.pow(r, 2);
+
+        System.out.println(a);
+        
+        scanner.close();
+    }
+}
