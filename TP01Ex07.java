b9c2f7ffe27481bdd43e92f05bac314ef3b97156
Júlia <juliavvitoria376@gmail.com>
2026-03-20 12:53:49 -0300

    Ex07: média geométrica

--- TP01Ex07.java
+++ TP01Ex07.java
@@ -2,3 +2,28 @@
  quaisquer que serão digitados.
   
  Dupla: Júlia Vitória e Lucas Antônio*/
+
+  import java.util.Scanner;
+
+  public class TP01Ex07 {   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Digite primeiro valor:");
+        double v1 = scanner.nextDouble();
+
+        System.out.println("Digite segundo valor valor:");
+        double v2 = scanner.nextDouble();
+
+        double r = Math.pow(v1 * v2, 1.0 / 2.0);
+
+        System.out.println(r);
+        
+        
+
+        
+        
+        scanner.close();
+    }
+}
