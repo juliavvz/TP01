6c3c1024ca5b52ac5ddc6d9a58361e2fd14e83cd
Júlia <juliavvitoria376@gmail.com>
2026-03-20 12:12:02 -0300

    Ex01: área do retângulo

--- TP01Ex01.java
+++ TP01Ex01.java
@@ -2,3 +2,26 @@
 calcular e exibir sua área
 
 Dupla: Júlia Vitória e Lucas Antônio*/
+
+
+import java.util.Scanner;
+
+public class TP01Ex01 {   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("digite base:");
+        double b = scanner.nextDouble();
+
+        System.out.println("digite altura:");
+        double a = scanner.nextDouble();
+
+        double r = b * a;
+
+        System.out.println(r);
+        
+        
+        scanner.close();
+    }
+}
