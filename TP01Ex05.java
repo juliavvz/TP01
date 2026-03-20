f03b2c61858809e88f81fa5772c9971009cdeec2
Júlia <juliavvitoria376@gmail.com>
2026-03-20 12:42:03 -0300

    Ex05: volume da esfera pelo diâmetro

--- TP01Ex05.java
+++ TP01Ex05.java
@@ -2,3 +2,24 @@
  de seu diâmetro que será digitado.
  
  Dupla: Júlia Vitória e Lucas Antônio*/
+
+ import java.util.Scanner;
+
+  public class TP01Ex05 {   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("digite diâmetro da esfera:");
+        double d = scanner.nextDouble();
+        
+        double ra = d / 2;
+
+        double r = (4.0 / 3.0) * 3.14 * Math.pow(ra, 3);
+
+        System.out.println(r);
+        
+        
+        scanner.close();
+    }
+}
