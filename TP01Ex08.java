c8fec61c6825b55e3c4e155110726d9b600984a4
rapaduragames <antilacus@gmail.com>
2026-03-23 16:19:32 -0300

    Ex08: Medida em quilometros

---
+++ TP01Ex08.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TP01Ex08 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("milha marítima:");
+        double m = sc.nextDouble();
+
+        System.out.println("Medida em quilometros: " + (m * 1.852));
+    }
+}
