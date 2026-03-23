28b6e4e9cbbf2fb1218e2a8d6a3e8f69daf68c22
rapaduragames <antilacus@gmail.com>
2026-03-23 16:27:27 -0300

    Ex10: Celsius para fahrenheit

---
+++ TP01Ex10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TP01Ex10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("graus Celsius,");
+         double C = sc.nextDouble();
+
+        System.out.println("fahrenheit: " + (C * 9/5 + 32));
+    }
+}
